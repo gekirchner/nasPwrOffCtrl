ab30eb5f982af55373240a11ec8ef6e8136bb34d
Georg Kirchner <71510288+gekirchner@users.noreply.github.com>
2021-05-07 09:45:36 +0200

    typo in comment of example file

--- pwrOff_if_no_Dev_config_example.sh
+++ pwrOff_if_no_Dev_config_example.sh
@@ -4,7 +4,7 @@
 
 #For usage of the script pwrOff_if_no_Dev.sh, the following has to be done:
 #   - make a copy of this file (pwrOff_if_no_Dev_config_example.sh) and name the copy "pwrOff_if_no_Dev_config.sh"
-#     (The configuration file "pwrOff_if_no_Dev_config.sh" has to be stored in the same folder as the script "pwrOff_if_no_Dev.sh" exists)
+#     (The configuration file "pwrOff_if_no_Dev_config.sh" has to be stored in the same folder in which the script "pwrOff_if_no_Dev.sh" exists)
 #   - edit the copy by changing the entries of DEVICE_IP_LIST to your environment: Enter all your devices which are connected to the NAS
 
 #               my Device #1:     my Device #2:     my Device #3      my Device #4:   ... my Device #n:
